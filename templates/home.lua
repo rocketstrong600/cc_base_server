7ae175f13fcdbc8780d8825be8802e10ad0631a6
Ryanthekid22 <Formiatti@gmail.com>
2025-03-06 03:48:47 +1000

    Testing local file loading

--- templates/home.lua
+++ templates/home.lua
@@ -33,85 +33,7 @@ local function openProgram(page, title, maxW, maxH, x, y)
         f:addProgram()
             :setSize("parent.w -1","parent.h -2")
             :setPosition(1, 2)
-            :execute(function() ---TEMPORARY
-
-                local main = Basalt.createFrame():setBackground(colors.white)
-
-                local idInput = ""
-                local passwordInput = ""
-                local url = "http://celtis.alcorlabs:35535/auth"
-
-                local function sendUserData(user, password, url)
-
-                    local data = {
-                        user = user,
-                        pass = password
-                    }
-
-                    local jsonData = textutils.serializeJSON(data)
-
-                    local response, err = http.post(url, jsonData, { ["Content-Type"] = "application/json"})
-
-                    if not response then
-                        Basalt.debug("HTTP request failed: " .. (err or "unknown error"))
-                        return nil
-                    end
-
-                    if response then
-                        local body = response.readAll()
-                        response.close()
-                        local table = textutils.unserializeJSON(body).pages
-                        return table
-                    else
-                        Basalt.debug("Error: No response received.")
-                    end
-                end
-
-                local screenWidth, screenHeight = term.getSize();
-                local padding = PaddingTool.PadWindowPercent(screenWidth, screenHeight, 100, 100, 10, 10, 33, 33)
-
-                local usrId = main:addInput()
-                    :setInputType("text")
-                    :setDefaultText("Staff ID")
-                    :setInputLimit(10)
-                    :setPosition(padding[1], padding[2])
-                    :setSize(padding[3], 1)
-                    :setBackground(colors.black)
-                    :setForeground(colors.lightGray)
-                    :onChange(function(text)
-                    idInput = text.getValue()
-                end)
-
-                local usrPswd = main:addInput()
-                    :setInputType("password")
-                    :setDefaultText("Password")
-                    :setInputLimit(10)
-                    :setPosition (padding[1],  padding[2] + 2)
-                    :setSize(padding[3], 1)
-                    :setBackground(colors.black)
-                    :setForeground(colors.lightGray)
-                    :onChange(function(text)
-                    passwordInput = text.getValue()
-                end)
-
-                local submitBtn = main:addButton()
-                    :setText("submit")
-                    :setSize(padding[3], 1)
-                    :setPosition(padding[1], padding[2] + 4)
-                    :setBackground(colors.lightGray)
-                    :setForeground(colors.black)
-                    :onClick(function()
-
-                    local result = sendUserData(idInput, passwordInput, url)
-
-                    if result then
-                        -- Process the result if the function returned successfully
-                        Basalt.debug(result)
-                        os.queueEvent("login_success", result)
-                    end
-                end)
-
-            end)
+            :execute(page)
             :onDone(function()
                 f:remove()
             end)
@@ -120,7 +42,7 @@ end
 
 local loginPage = PageLoader.requestPage("http://celtis.alcorlabs.com:35535/login")
 if loginPage then
-    openProgram(loginPage, "Sign In", 30, 12)
+    openProgram("alcor/login", "Sign In", 30, 12)
 end
 
 local function initEnv(tabs)
